f82ec8fe8eb8ea6dbe0f437c833ee8eee3ab2e11
Harman Sran <iharmandeep@gmail.com>
2021-12-04 15:14:53 -0500

    Change /project -> /lyrics in Gatsby paths

--- web/src/components/project.js
+++ web/src/components/project.js
@@ -57,7 +57,7 @@ function Project(props) {
                   {relatedProjects.map(project => (
                     <li key={`related_${project._id}`}>
                       {project.slug ? (
-                        <Link to={`/project/${project.slug.current}`}>{project.title}</Link>
+                        <Link to={`/lyrics/${project.slug.current}`}>{project.title}</Link>
                       ) : (
                         <span>{project.title}</span>
                       )}
